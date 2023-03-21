e2f13728ee074485eb22d1a7023c26f3b29068ab
hoangthang1003 <110215092+hoangthang1003@users.noreply.github.com>
2023-03-21 13:34:42 +0700

    update

--- customer/templates/customer.js
+++ customer/templates/customer.js
@@ -7,48 +7,166 @@ function loadCustomers(page) {
             contentType: 'application/json',
         },
         success: function (data) {
-            renderCustomers(data);
+            renderCustomers(data.content);
+            renderPage(data);
         },
         error: function (error) {
             console.log(error);
         }
     });
 }
-function showById(id) {
-    $.ajax({
-        type:"GET",
-        url:`http://localhost:8080/customer/` + id,
-        header:{
-            contentYpe : 'application/json',
-        },
-        success: function (data) {
 
+function movePage(nextPage) {
+    loadCustomers(nextPage);
+}
+
+function renderPage(customers) {
+    let page = "";
+    if (customers.number == customers.totalPages - 1 && customers.number > 0) {
+        page += `
+    <button class="page-item btn btn-primary btn-sm" 
+    onclick="movePage(${customers.number - 1})">
+    Before
+    </button>
+    `
+    }
+    for (let i = 1; i <= customers.totalPages; i++) {
+        let pageItem = $(`<button class="page-item number btn btn-primary btn-sm"
+                      onclick="movePage(${i - 1})">
+                      ${i}
+                      </button>`);
+        if (i === customers.number + 1) {
+            pageItem.addClass("active");
+        } else {
+            pageItem.removeClass("active");
+        }
+        page += pageItem.prop('outerHTML');
+    }
+
+    if (customers.number == 0 && customers.number < customers.totalPages) {
+        page += `
+    <button class="page-item btn btn-primary btn-sm" 
+    onclick="movePage(${customers.number + 1})">
+    Next
+    </button>
+    `
+    }
+    $("#paging").html(page);
+}
+
+
+function getCustomerInfoUpdate(id) {
+
+    $.ajax({
+        headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json",
         },
-        error: function (error) {
+        type: "get",
+        url: `http://localhost:8080/customer/`+ id,
+        success: function (data) {
+            listAllType();
+            let element = "";
+            let customer = data;
+            element +=
+                `
+             <div>
+                        <input type="hidden" id="id1" value="${customer.id}">
+                        <br>
+                        <label>Tên khách hàng</label>
+                        <input type="text" id="name1" placeholder="Nhập tên" required value="${customer.customer}">
+                        <br>
+                        <label>Địa chỉ</label>
+                        <input type="text" id="address1" placeholder="Nhập địa chỉ" required value="${customer.address}">
+                        <br>
+                        <label>CMND</label>
+                        <input type="text" id="idCard1" placeholder="Nhập CCCD" required value="${customer.idCard}">
+                        <br>
+                        <label>SDT</label>
+                        <input type="text" id="phoneNumber1" placeholder="Nhập SDT" required value="${customer.phoneNumber}">
+                        <br>
+                        <label>Ngày sinh</label>
+                        <input type="date" id="birth1" placeholder="Nhập ngày sinh" required value="${customer.dateOfBirth}">
+                        <br>
+                        <label>Img</label>
+                        <input type="text" id="img1" value="${customer.img}">
+                        <br>
+                        <label>Kiểu khách hàng</label>
+                        <div class="customerTypeForm"></div>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-dismiss="modal">No</button>
+                        <button type="submit" class="btn btn-danger">Save</button>
+                    </div>
+      `
+            $("#edit-form").html(element);
+        },
+        error: function(error) {
             console.log(error);
         }
     })
 }
+function editCustomer(id,name,address,idCard,phoneNumber,birth ,img,customerType) {
+    debugger
+    $.ajax({
+        type: "POST",
+        url: `http://localhost:8080/customer/`+ id,
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        data: JSON.stringify(
+            {
+                id:id,
+                customer:name,
+                dateOfBirth: birth,
+                address: address,
+                idCard: idCard,
+                phoneNumber: phoneNumber,
+                img: img,
+                customerType: {id:customerType}
+            }
+        ),
+        success: () => {
 
+            alert("Thay đổi thông tin khách hàng thành công!");
+            $('#modelId').hide();
+            $('body').removeClass('modal-open');
+            $('.modal-backdrop').remove();
+            loadCustomers()
+
+        },
+        error: () => {
+            alert("lỗi không thêm được");
+        }
+    })
+}
+
+$('#editCustomerForm').submit(() =>{
+
+    let id = $("#id1").val();
+    let name = $("#name1").val();
+    let address = $("#address1").val();
+    let idCard = $("#idCard1").val();
+    let phoneNumber = $("#phoneNumber1").val();
+    let birth = $("#birth1").val();
+    let img = $("#img1").val();
+    let customerType = $("#customerType").val();
+    editCustomer(id,name,address,idCard,phoneNumber,birth,img,customerType);
+})
 
 
 $(document).ready(function () {
     loadCustomers();
     listAllType();
-    showById()
+    // showById()
 })
-function renderCustomerById(customer) {
-    let element = "";
 
-
-}
 
 function renderCustomers(customers) {
     let element = "";
     for (let customer of customers) {
         element += `
-
-
       <tr>
         <td>${customer.id}</td>
         <td>${customer.customer}</td>
@@ -60,7 +178,7 @@ function renderCustomers(customers) {
         <td><button type="button"
                 class="btn btn-primary"
                 data-toggle="modal" data-target="#exampleModalEdit"
-                                onclick="getCustomerInfo('${customer.id}', '${customer.customer}')"
+                                onclick="getCustomerInfoUpdate('${customer.id}')"
 
                 >
                Edit
@@ -150,7 +268,6 @@ function createCustomer(name,address,idCard,phoneNumber,birth ,img,customerType)
 }
 
 $('#createCustomerForm').submit(() =>{
-
     let name = $("#name").val();
     let address = $("#address").val();
     let idCard = $("#idCard").val();
@@ -179,13 +296,14 @@ const listAllType = () => {
 }
 
 const allCustomerType = (customerType) => {
+
     let element = `<select id="customerType">`
     for (let type of customerType) {
         element += `<option value="${type.id}">${type.name}</option>`
     }
     element += `</select>`;
 
-    $("#customerTypeForm").html(element);
+    $(".customerTypeForm").html(element);
 }
 
 
@@ -199,6 +317,7 @@ const allCustomerType = (customerType) => {
 //     <a href="#" class="btn btn-primary">Add</a>
 //   </div>
 // </div>
+
 
 
 
